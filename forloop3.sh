72e663963c42f2c7fbdaaa382491ccb3b2ba864f
kalyangoud145 <65416828+kalyangoud145@users.noreply.github.com>
2020-10-16 19:46:04 +0530

    Update forloop3.sh

--- forloop3.sh
+++ forloop3.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 read -p "Enter the number " n
+echo "welcome message kalyan"
 flag=0
 for (( i=2;i <= $n/2 ;i++))
 do
 	if [ $(($n%i)) -eq 0 ]
 	then
-		flag=1
-		echo " $n is not a prime number "
+		flag=1;
+		echo " $n is not a prime number ";
 		exit
 	fi
 done
